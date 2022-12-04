b02e72f34d83a334f952298402af05eccbcfbdb1
Waveyyyy <waves-sec@protonmail.com>
2022-12-04 16:35:54 +0000

    Explicitly close the file within the fIn function

--- AoC/Utilities.cpp
+++ AoC/Utilities.cpp
@@ -4,10 +4,13 @@ std::istream_iterator<std::string> fIn(const char* fPath)
 {
 	std::ifstream f{ fPath };
 
-	if (!f.is_open())
+	if (f.is_open())
 	{
 		std::cout << "Failed to open file " << fPath << std::endl;
 		return std::istream_iterator<std::string>{};
 	}
-	return std::istream_iterator<std::string>{f};
+	auto it = std::istream_iterator<std::string>{f};
+
+	f.close();
+	return it;
 }
