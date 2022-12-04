53d100a8fd0aa7bbda27716e626bc3cd821ef68b
Waveyyyy <waves-sec@protonmail.com>
2022-12-04 15:43:56 +0000

    Implemented basic file reading.
    Currently prints each line as it is read

--- AoC/Utilities.cpp
+++ AoC/Utilities.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
 #include "Utilities.h"
+
+void fIn(const char* fPath)
+{
+	std::ifstream f{ fPath };
+
+	if (!f.is_open())
+	{
+		std::cout << "Failed to open file" << fPath << std::endl;
+		return;
+	}
+
+	std::string line;
+	while (std::getline(f, line))
+	{
+		std::cout << line << std::endl;
+	}
+
+	f.close();
+}
--- AoC/Utilities.h
+++ AoC/Utilities.h
@@ -1,1 +1,3 @@
 #pragma once
+
+void fIn(const char* fPath);
