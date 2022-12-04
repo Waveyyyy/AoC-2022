5b212c574eec4e5f4ad18dedc947cbe770fb55df
Waveyyyy <waves-sec@protonmail.com>
2022-12-04 16:37:40 +0000

    fix logic issue when checking if file is open
    Error handling is now done outside of the if statement which checks if the file is open.

--- AoC/Utilities.cpp
+++ AoC/Utilities.cpp
@@ -6,11 +6,11 @@ std::istream_iterator<std::string> fIn(const char* fPath)
 
 	if (f.is_open())
 	{
-		std::cout << "Failed to open file " << fPath << std::endl;
-		return std::istream_iterator<std::string>{};
-	}
-	auto it = std::istream_iterator<std::string>{f};
+		auto it = std::istream_iterator<std::string>{f};
 
-	f.close();
-	return it;
+		f.close();
+		return it;
+	}
+	std::cout << "Failed to open file " << fPath << std::endl;
+	return std::istream_iterator<std::string>{};
 }
