16d78002f1497de0ef15622a458ff5b451e2285d
Waveyyyy <waves-sec@protonmail.com>
2022-12-06 20:08:39 +0000

    Comments added to newly written code

--- AoC/Utilities.cpp
+++ AoC/Utilities.cpp
@@ -43,24 +43,39 @@ std::vector<std::string> rFile_winapi(const char* fPath)
 	DWORD bRead;		// Number of bytes actually read
 	std::vector<std::string> lines;
 	
+	// Read the file one character at a time until there is nothing left to read
 	while (ReadFile(fHandle, &c, sizeof(c), &bRead, NULL) && bRead > 0)
 	{
+		// Append the current character to the current line
 		line.push_back(c);
+
+		// Check if the end of the line has been reached
 		if (c != '\r')
 		{
+			//If not, continue to the next character
 			continue;
 		}
+		// If so, read the next character (newline)
 		if (ReadFile(fHandle, &c, sizeof(c), &bRead, NULL) && bRead > 0)
 		{
+			// Append the newline to complete the cr-lf return
 			line.push_back(c);
+
+			// Check if the next character is a newline
 			if (c != '\n')
 			{
+				// If not, continue to the next character
 				continue;
 			}
+
+			// If the end of the line has been reached, 
+			// store the line in a vector
 			lines.push_back(line);
 			line.clear();
 		}
 	}
+
+	// Close the file before returning
 	CloseHandle(fHandle);
 
 	return lines;
