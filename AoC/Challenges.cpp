0ae66383b9d96aca049fdf023c35c27db4770d67
Waveyyyy <waves-sec@protonmail.com>
2022-12-06 19:44:31 +0000

    Added DayOne function
    this *should* work, rFile_winapi is not reading files line by line which is breaking it.

--- AoC/Challenges.cpp
+++ AoC/Challenges.cpp
@@ -3,16 +3,34 @@
 
 void DayOne()
 {
-	std::istream_iterator<std::string> file = string_IS_iter("Day1.txt");
+	struct elf {
+		int num;
+		int calories;
+	};
 
+	auto lines = rFile_winapi("Day1.txt");
+	elf winner{0,0};
+	elf current{1,0};
 
-
-	while (file != std::istream_iterator<std::string>{})
+	for (const auto& line : lines)
 	{
+		if (line.empty() || line.find("\r\n") == 0 && line.size() == 2)
+		{
 
-		std::cout << *file << std::endl;
-			
-		file++;
+			if ((winner.calories < current.calories) && (winner.num != current.num))
+			{
+				winner.num = current.num;
+				winner.calories = current.calories;
+			}
+
+			//std::cout << "elf No." << current.num << " has " << current.calories << " calories" << std::endl;
+			//std::cout << "Current winner is elf No." << winner.num << " with " << winner.calories << " calories" << std::endl;
+			current.num += 1;
+			current.calories = 0;
+			continue;
+		}
+		current.calories += std::atoi(line.c_str());
 
 	}
+	std::cout << "Winner is elf No." << winner.num << " with " << winner.calories << " calories" << std::endl;
 }
