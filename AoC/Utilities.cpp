3467a708cd5db0a474479334237c938ac2ab6b4a
Waveyyyy <waves-sec@protonmail.com>
2022-12-06 18:21:41 +0000

    Use windows api to read files
    I want to learn more about the windows api, so will try to use it as much as possible in AoC

--- AoC/Utilities.cpp
+++ AoC/Utilities.cpp
@@ -19,3 +19,29 @@ std::istream_iterator<std::string> string_IS_iter(const char* fPath)
 	std::cout << "Failed to open file " << fPath << std::endl;
 	return std::istream_iterator<std::string>{};
 }
+
+std::vector<std::string> rFile_winapi(const char* fPath)
+{
+	std::wstring wfPath(fPath, fPath + strlen(fPath));
+	LPCWSTR lpfPath = wfPath.c_str();
+
+	HANDLE fHandle = CreateFile(lpfPath, GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+	if (fHandle == INVALID_HANDLE_VALUE)
+	{
+		std::cout << "Failed to open the file!" << std::endl;
+		return std::vector<std::string>{};
+	}
+
+	char buf[1024];
+	DWORD bRead;
+	std::vector<std::string> lines;
+	
+	while (ReadFile(fHandle, buf, sizeof(buf), &bRead, NULL) && bRead > 0)
+	{
+		lines.push_back(std::string(buf, bRead));
+	}
+	CloseHandle(fHandle);
+
+	return lines;
+}
--- AoC/Utilities.h
+++ AoC/Utilities.h
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <string>
 #include <iterator>
+#include <vector>
+#include <windows.h>
 
 // Returns an iterator that can be used to iterate over the lines in the file
 // at the given path.
@@ -13,3 +15,4 @@
 // be opened.
 std::istream_iterator<std::string> string_IS_iter(const char* fPath);
 
+std::vector<std::string> rFile_winapi(const char* fPath);
